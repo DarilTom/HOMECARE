d3199ede198fcd651e0c7dd552a5bb1c91a3cbdb
Sebastainjoseph <sebastianjosephjv@gmail.com>
2025-02-21 18:26:56 +0530

    service list push cheythu

--- homecare/homecare_app/views.py
+++ homecare/homecare_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Service
 
-# Create your views here.
+# Function to get all services
+def service_list(request):
+    services = Service.objects.all()  # Fetch all services from the database
+    return render(request, 'service_list.html', {'services': services})
