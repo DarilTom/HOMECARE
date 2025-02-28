1db98658414ea60916e065531bfe00df9425833f
jesvin1010 <jesvins27@gmail.com>
2025-03-01 00:00:48 +0530

    signup done again

--- homecare/homecare_app/urls.py
+++ homecare/homecare_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signup, user_login, home, user_logout, worker_profile
+from .views import signup, signup_page, user_login, home, user_logout, worker_profile
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -15,6 +15,7 @@ urlpatterns = [
 #   25-1-25
    path('worker/profile/<int:worker_id>/', views.worker_myprofile, name='worker_myprofile'),
    path("signup/",signup, name="signup"),
+   
 #28-2-25
 path('worker/<int:worker_id>/bookings/',views.worker_booking_list, name='worker_booking_list'),
 
